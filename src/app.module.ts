70df1a8605e1d5f50144577ee43beb459e58985f
drummond-tech-admin <107729719+drummond-tech-admin@users.noreply.github.com>
2022-09-29 19:04:07 -0400

    feat: mysql with sequilize integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controller';
+import { Livro } from './livro.model';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
